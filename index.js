613cd8135cd6c58c997199568dc8571cf0cb9ab0
github username <github email address>
2019-04-02 11:28:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
+document.getElementById('input')
+
+const inputTag = document.getElementById('input')
+
 function addingEventListener() {
+inputTag.addEventListener('click', function(event){
+  alert('I was Clicked!')
+})
 }
