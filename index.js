25a6c67a16cfeef147c44dd03d79a8f3799b96d0
campbelllssoup <matjocampbell@gmail.com>
2019-04-02 00:02:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+let input = document.querySelector('#input');
+
 function addingEventListener() {
+
+  function eventAlert() {
+    alert("Click Successful");
+  }
+
+  input.addEventListener('click', eventAlert);
 }
+
+addingEventListener();
